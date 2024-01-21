2f1c143292387f6e5c459c482e692d4c4ab47323
ido evergeen <dicksonevergreenido@gmail.com>
2024-01-21 17:00:00 +0100

    add new note

--- app/components/foldersidebar.tsx
+++ app/components/foldersidebar.tsx
@@ -1,33 +1,58 @@
+import useSupabaseBrowser from "@/utils/supabase-browser";
+import {
+  useInsertMutation,
+  useMutateItem,
+} from "@supabase-cache-helpers/postgrest-react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import React from "react";
 import { RotatingLines } from "react-loader-spinner";
+import { TypedSupabaseClient } from "@/utils/types";
 
 export default function FolderSidebar({
+  refetch,
   folders,
   selectedFolder,
   setSelectedFolder,
   setFolders,
   isLoading,
   setNote,
-  note
+  note,
 }) {
   console.log(folders, "folders");
 
-  console.log(note)
+  console.log(note);
+
+  console.log(selectedFolder, "testt");
+  const queryClient = useQueryClient();
+  const supabase = useSupabaseBrowser();
+  const mutation = useMutation({
+    mutationFn: (newTodo) => {
+      return supabase.from("folders").insert({
+        name: "new folder2",
+      });
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["folders"] });
+    },
+  });
+https://www.youtube.com/watch?v=H-sFluixjjQ&pp=ygUTbmlnZXJpYSB2cyBjYW1lcm9vbg%3D%3D
+  const mutationnotes = useMutation({
+    mutationFn: (newTodo) => {
+      return supabase.from("notes").insert({
+      content: 'gghghhgg',
+      folderid : selectedFolder
+,
+title : 'new'      });
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["notes"] });
+    },
+  });
+
   const handleCreateFolder = () => {
-    setFolders([...folders, { id: 10, name: "evergreen" }]);
-    setNote([...note,
-        
-        {
-            id: 10,
-            folderId: {
-              id: 10,
-              title: "evergreen",
-            },
-            title: "testing",
-            content: "Milk, eggs, bread",
-          },
-    ]
-        )
+    mutation.mutate();
   };
 
   return (
@@ -67,7 +92,14 @@ export default function FolderSidebar({
           </svg>
         </div>
 
-        <button className="font-semibold h-[40px]  items-center flex rounded justify-center w-full text-white bg-[#1C1C1C] mb-[30px] p-[10px]">
+        <button
+        
+        onClick={()=>{
+
+          mutationnotes.mutateAsync()
+        }}
+        
+        className="font-semibold h-[40px]  items-center flex rounded justify-center w-full text-white bg-[#1C1C1C] mb-[30px] p-[10px]">
           <svg
             className="mr-2"
             width="21"
@@ -97,71 +129,78 @@ export default function FolderSidebar({
 
       <div className="mb-[8px] flex justify-between text-white p-[20px] items-center">
         <h2>Folders</h2>
-        <svg
-          className="cursor-pointer "
-          onClick={() => handleCreateFolder()}
-          width="20"
-          height="20"
-          viewBox="0 0 20 20"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g opacity="0.4">
-            <path
-              d="M3.33332 16.6667H16.6667C17.1087 16.6667 17.5326 16.4911 17.8452 16.1785C18.1577 15.866 18.3333 15.442 18.3333 15V6.66667C18.3333 6.22464 18.1577 5.80072 17.8452 5.48816C17.5326 5.17559 17.1087 5 16.6667 5H10.0583C9.78381 4.99858 9.5139 4.92937 9.27257 4.79853C9.03124 4.66769 8.82597 4.47927 8.67499 4.25L7.99166 3.25C7.84068 3.02073 7.6354 2.83231 7.39407 2.70147C7.15275 2.57063 6.88283 2.50142 6.60832 2.5H3.33332C2.8913 2.5 2.46737 2.67559 2.15481 2.98816C1.84225 3.30072 1.66666 3.72464 1.66666 4.16667V15C1.66666 15.9167 2.41666 16.6667 3.33332 16.6667Z"
-              stroke="white"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M10 8.33331V13.3333"
-              stroke="white"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M7.5 10.8333H12.5"
-              stroke="white"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </g>
-        </svg>
+
+        {mutation.isPending ? (
+          <RotatingLines width="30" strokeWidth="2" strokeColor="white" />
+        ) : (
+          <svg
+            className="cursor-pointer "
+            onClick={() => handleCreateFolder()}
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g opacity="0.4">
+              <path
+                d="M3.33332 16.6667H16.6667C17.1087 16.6667 17.5326 16.4911 17.8452 16.1785C18.1577 15.866 18.3333 15.442 18.3333 15V6.66667C18.3333 6.22464 18.1577 5.80072 17.8452 5.48816C17.5326 5.17559 17.1087 5 16.6667 5H10.0583C9.78381 4.99858 9.5139 4.92937 9.27257 4.79853C9.03124 4.66769 8.82597 4.47927 8.67499 4.25L7.99166 3.25C7.84068 3.02073 7.6354 2.83231 7.39407 2.70147C7.15275 2.57063 6.88283 2.50142 6.60832 2.5H3.33332C2.8913 2.5 2.46737 2.67559 2.15481 2.98816C1.84225 3.30072 1.66666 3.72464 1.66666 4.16667V15C1.66666 15.9167 2.41666 16.6667 3.33332 16.6667Z"
+                stroke="white"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M10 8.33331V13.3333"
+                stroke="white"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M7.5 10.8333H12.5"
+                stroke="white"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </g>
+          </svg>
+        )}
       </div>
 
-{
-  isLoading ? <RotatingLines
-  visible={true}
-  height="96"
-  width="96"
-  color="grey"
-  strokeWidth="5"
-  animationDuration="0.75"
-  ariaLabel="rotating-lines-loading"
-  wrapperStyle={{}}
-  wrapperClass=""
-  />
- : null
-}
-
-
+      {isLoading ? (
+        <div className="flex justify-center items-center">
+          <RotatingLines
+            visible={true}
+            width="50"
+            strokeColor="white"
+            strokeWidth="3"
+            animationDuration="0.75"
+            ariaLabel="rotating-lines-loading"
+          />
+        </div>
+      ) : null}
 
       {folders?.map((i) => {
         return (
           <div
             onClick={() => {
-              setSelectedFolder(i.id);
+
+              if (i.id) {
+                setSelectedFolder(i.id);
+          
+              }
+         
             }}
             className={`text-xl cursor-pointer text-white  
-          ${selectedFolder === i.id && "bg-[#312EB5]"}
+          // ${selectedFolder === i.id && "bg-[#312EB5]"}
           
           p-3`}
             key={i.id}
           >
             {i.name}
+            {i.id}
           </div>
         );
       })}
--- app/components/notesidebar.tsx
+++ app/components/notesidebar.tsx
@@ -3,27 +3,36 @@ import React from "react";
 import { folderList } from "./sidebar";
 import { useParams } from "next/navigation";
 import { stringify } from "querystring";
+import { getNotes } from "@/queries/get-notes";
+import useSupabaseBrowser from "@/utils/supabase-browser";
+import { useQuery } from "@supabase-cache-helpers/postgrest-react-query";
+import { RotatingLines } from "react-loader-spinner";
 
-export default function NoteSidebar({ notes }) {
-  const title = notes && notes[0];
-
-  const { id } = useParams();
-
-  console.log(id);
-  const selectedNote = notes.find((note) => note.id.toString() === id);
-
-  console.log(selectedNote);
+export default function NoteSidebar({ notes, selectedFolder, isLoading }) {
   return (
     <div>
       <div className="w-[350px] py-[30px] h-screen     px-5 mb-20 bg-[#1C1C1C]">
         <h1 className="text-[22px] font-semibold text-white mb-[30px] capitalize">
-          {title?.folderId.title}
+          {/* {title?.folderId.title} */}
         </h1>
-        {notes.map((i) => {
-          return (
-            <NoteItem id={i.id} key={i.id} title={i.title} desc={i.content} />
-          );
-        })}
+        {isLoading ? (
+          <div className="flex justify-center items-center">
+            <RotatingLines
+              visible={true}
+              width="30"
+              strokeColor="white"
+              strokeWidth="3"
+              animationDuration="0.75"
+              ariaLabel="rotating-lines-loading"
+            />
+          </div>
+        ) : (
+          notes?.map((i) => {
+            return (
+              <NoteItem id={i.id} key={i.id} title={i.title} desc={i.content} />
+            );
+          })
+        )}
       </div>
     </div>
   );
@@ -32,12 +41,10 @@ export default function NoteSidebar({ notes }) {
 const NoteItem = ({ title, desc, id }) => {
   const paaramsid = useParams();
 
-  console.log(paaramsid);
-
-  console.log(id);
-
   return (
     <Link href={`/note/${id}`}>
+      {id}
+      {title}
       <div
         className={` 
         
