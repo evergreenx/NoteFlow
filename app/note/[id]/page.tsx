139bc81f6aad75c2e2374cbfd23a4cf5a9925755
ido evergeen <dicksonevergreenido@gmail.com>
2024-01-28 03:24:45 +0100

    add note show area

--- app/note/[id]/page.tsx
+++ app/note/[id]/page.tsx
@@ -2,5 +2,21 @@ import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import React from "react";
 
 export default function Page({ params }: { params: { id: string } }) {
-  return <div className="text-8xl">{params.id}</div>;
+  return <div className="text-8xl p-[50px]">
+    
+    
+    
+    <h1 className="text-[32px] font-semibold">
+
+      fdfdf
+    </h1>
+    
+    
+    {params.id}
+    
+    
+    
+    
+    
+    </div>;
 }
