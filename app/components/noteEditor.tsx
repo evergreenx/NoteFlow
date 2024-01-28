eea2ab273a306391ceba781a0e7a818a00a027be
ido evergeen <dicksonevergreenido@gmail.com>
2024-01-28 11:05:38 +0100

    add editor

--- app/components/noteEditor.tsx
+++ app/components/noteEditor.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { Editor } from "react-draft-wysiwyg";
+import dynamic from "next/dynamic";
+
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+import { EditorState, convertToRaw } from "draft-js";
 
 export default function NoteEditor() {
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+
+  const onEditorStateChange = (editorState) => {
+    setEditorState(editorState);
+    return console.log(editorState);
+  };
   return (
-    <div>N</div>
-  )
+    <div>
+      
+      <Editor
+      spellCheck
+        editorState={editorState}
+        toolbarClassName="toolbarClassName"
+        wrapperClassName="wrapperClassName"
+        editorClassName="editorClassName"
+        onEditorStateChange={onEditorStateChange}
+      />
+    </div>
+  );
 }
